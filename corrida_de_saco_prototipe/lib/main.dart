382092df67894f26e1bfbef7fc3f5ecb6898128a
andriel <andrielferreira@outlook.com.br>
2018-11-27 15:49:34 -0200

    Tela Tutorial 1/3

--- corrida_de_saco_prototipe/lib/main.dart
+++ corrida_de_saco_prototipe/lib/main.dart
@@ -1,93 +1,103 @@
+import 'package:corrida_de_saco_prototipe/screens/tutorial.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(HomePage());
+void main(){
+  runApp(MaterialApp(
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+
+
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        
         bottomAppBarColor: Color.fromRGBO(200, 100, 100, 1),
         buttonColor: Color.fromRGBO(166, 166, 166, 1),
         scaffoldBackgroundColor: Color.fromRGBO(255, 224, 204, 1),
         primaryColor: Color.fromRGBO(200, 100, 100, 1),
       ),
-      title: "Corrida de Saco",
-      home: Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            height: 50.0,
-          ),
+      title: "Corrida de saco",
+    ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50.0,
         ),
-        appBar: AppBar(
-          title: Text(
-            "Corrida de saco",
-            style: TextStyle(fontSize: 30.0),
-          ),
-          centerTitle: true,
+      ),
+      appBar: AppBar(
+        title: Text(
+          "Corrida de saco",
+          style: TextStyle(fontSize: 30.0),
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 20.0),
-                child: Image.asset("assets/corrida.jpg"),
-              ),
-              Container(
-                height: 50.0,
-                width: 200.0,
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Como Jogar",
-                    style: TextStyle(
-                        fontSize: 20.0, fontWeight: FontWeight.normal),
-                  ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 20.0),
+              child: Image.asset("assets/corrida.jpg"),
+            ),
+            Container(
+              height: 50.0,
+              width: 200.0,
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => TutorialPage()));
+                },
+                child: Text(
+                  "Como Jogar",
+                  style:
+                      TextStyle(fontSize: 20.0, fontWeight: FontWeight.normal),
                 ),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 50.0,
-                width: 200.0,
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Entrar",
-                    style: TextStyle(
-                        fontSize: 20.0, fontWeight: FontWeight.normal),
-                  ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 50.0,
+              width: 200.0,
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  "Entrar",
+                  style:
+                      TextStyle(fontSize: 20.0, fontWeight: FontWeight.normal),
                 ),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 50.0,
-                width: 200.0,
-                child: RaisedButton(
-                  color: Colors.blueAccent,
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Login com ",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.normal),
-                      ),
-                      Image.asset(
-                        "assets/face.png",
-                        height: 35.0,
-                      ),
-                    ],
-                  ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 50.0,
+              width: 200.0,
+              child: RaisedButton(
+                color: Colors.blueAccent,
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Login com ",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.normal),
+                    ),
+                    Image.asset(
+                      "assets/face.png",
+                      height: 35.0,
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
---
+++ corrida_de_saco_prototipe/lib/screens/tutorial.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+class TutorialPage extends StatefulWidget {
+  _TutorialPageState createState() => _TutorialPageState();
+}
+
+class _TutorialPageState extends State<TutorialPage> {
+  PageController _pageController =
+      PageController(initialPage: 0, keepPage: false);
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _page = _pageController.page.toInt();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tutorial"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.redAccent,
+                onPressed: (_page != 0)
+                    ? () {
+                        setState(() {
+                          _pageController.previousPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Cubic(2, 2, 2, 2));
+                          _page--;
+                        });
+                      }
+                    : null,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.navigate_before),
+                    Text("Anterior"),
+                  ],
+                ),
+              ),
+              Text(
+                "${_page} / 3",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              RaisedButton(
+                color: Colors.green,
+                onPressed: (_page != 2)
+                    ? () {
+                        setState(() {
+                          _pageController.nextPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Cubic(2, 2, 2, 2));
+                          _page++;
+                        });
+                      }
+                    : null,
+                child: Row(
+                  children: <Widget>[
+                    Text("Próximo"),
+                    Icon(Icons.navigate_next)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Este é seu personagem",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                Image.asset(
+                  "assets/menina.jpg",
+                  height: 180.0,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          height: 150.0,
+                          width: 150.0,
+                          child: Center(
+                            child: Text(
+                              "Controle a direção rotacionando o botão de direção",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0)),
+                              color: Colors.white,
+                              border: Border.all()),
+                        ),
+                        Icon(
+                          Icons.arrow_downward,
+                          size: 50.0,
+                        ),
+                        Image.asset(
+                          "assets/direcao.png",
+                          height: 90.0,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          height: 150.0,
+                          width: 150.0,
+                          child: Center(
+                            child: Text(
+                              "Pule pressionando o botão de pular",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0)),
+                              color: Colors.white,
+                              border: Border.all()),
+                        ),
+                        Icon(
+                          Icons.arrow_downward,
+                          size: 50.0,
+                        ),
+                        Image.asset(
+                          "assets/pulo.png",
+                          height: 90.0,
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(),
+          Container()
+        ],
+      ),
+    );
+  }
+}
