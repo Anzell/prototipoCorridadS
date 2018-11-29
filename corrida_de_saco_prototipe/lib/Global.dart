a6e624bf927e430e8e5e7f8ec0e3abe9a8850014
Rodrigo João Bertotti <digao520d@gmail.com>
2018-11-29 19:27:56 -0200

    Update Global.dart

--- corrida_de_saco_prototipe/lib/Global.dart
+++ corrida_de_saco_prototipe/lib/Global.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-/*
-* SINGLETON - VER O LINK ABAIXO PARA ENTENDER COMO FUNCIONA SINGLETON EM DART
-* https://stackoverflow.com/questions/12649573/how-do-you-build-a-singleton-in-dart
-* */
 class PreferenciasDoUsuario{
   static Color COR_TEMA = Colors.greenAccent;
   static bool COM_SOM = true;
